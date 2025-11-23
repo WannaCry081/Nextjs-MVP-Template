1c2f34b0dda5cb835d45a131ab61a5bfcc5e3780
Ly <liraedata59@gmail.com>
2025-11-23 23:08:17 +0800

    refactor: simplify Page component by removing authentication and user display logic

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,33 +1,7 @@
-"use client";
-import {
-  SignedIn,
-  UserButton,
-  useUser,
-  Protect,
-  PricingTable,
-} from "@clerk/nextjs";
-
 export default function Page() {
-  const { user } = useUser();
-
   return (
-    <div>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-
-      <Protect
-        condition={(has) => {
-          return !has({ plan: "free_user" });
-        }}
-        fallback={<PricingTable />}
-      >
-        <h1>Actual Features here</h1>
-
-        <p>You got scammmmeedddd! HAHA </p>
-      </Protect>
-
-      <h1>Hello world {user?.fullName}</h1>
+    <div className="flex items-center justify-center w-full h-full">
+      Dashboard Page
     </div>
   );
 }
