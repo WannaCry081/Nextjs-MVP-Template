d8e9ae0ab0f5fbc918e7e3ed4150b384e7980f19
Ly <liraedata59@gmail.com>
2025-11-23 16:33:50 +0800

    feat: wrap children in ClerkProvider for authentication context

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
+// Providers
+import { ClerkProvider } from "@clerk/nextjs";
+
+// Styles
 import "@/app/styles/globals.css";
 
 const geistSans = Geist({
@@ -28,7 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ClerkProvider>{children}</ClerkProvider>
       </body>
     </html>
   );
