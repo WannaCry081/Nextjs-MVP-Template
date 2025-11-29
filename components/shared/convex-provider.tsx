a3fa4495cab0edb77eaeeaa0d5716618bc507cc9
Ly <liraedata59@gmail.com>
2025-11-29 22:55:39 +0800

    feat: integrate ConvexQueryClient and QueryClientProvider for enhanced query management

--- components/shared/convex-provider.tsx
+++ components/shared/convex-provider.tsx
@@ -3,7 +3,9 @@
 import { PropsWithChildren } from "react";
 import { useAuth } from "@clerk/nextjs";
 import { ConvexReactClient } from "convex/react";
+import { ConvexQueryClient } from "@convex-dev/react-query";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
   throw new Error("Missing NEXT_PUBLIC_CONVEX_URL in your .env file");
@@ -12,9 +14,22 @@ if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
 
 export function ConvexClientProvider({ children }: PropsWithChildren) {
+  const convexQueryClient = new ConvexQueryClient(convex);
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        queryKeyHashFn: convexQueryClient.hashFn(),
+        queryFn: convexQueryClient.queryFn(),
+      },
+    },
+  });
+
+  convexQueryClient.connect(queryClient);
+
   return (
     <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-      {children}
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </ConvexProviderWithClerk>
   );
 }
