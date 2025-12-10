b9d3366d01095a004425fd6e1a03c140162c481b
Ly <liraedata59@gmail.com>
2025-12-10 10:35:37 +0800

    refactor: update provider imports and add ConvexClientProvider and ThemeProvider components

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 
 // Providers
 import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "@/components/shared/theme-provider";
-import { ConvexClientProvider } from "@/components/shared/convex-provider";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
 
 // Styles
 import "@/app/styles/globals.css";
